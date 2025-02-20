9157dbe0966e6fd23478e04845aae08b2cdbc170
sherif <sherifelgendy2004@gmail.com>
2025-02-20 09:44:23 +0200

    Market Task MVC

---
+++ Amazon/Data/AmazonContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Amazon.Models;
+
+    public class AmazonContext : DbContext
+    {
+        public AmazonContext (DbContextOptions<AmazonContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Amazon.Models.Product> Product { get; set; } = default!;
+
+        public DbSet<Amazon.Models.Category> Category { get; set; } = default!;
+    }
---
+++ Amazon/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Amazon.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required]
+        public double price { get; set; }
+        [Required]
+        public string DefaultImage { get; set; }
+
+        public string? Images { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category? Category { get; set; }
+
+    }
+}
